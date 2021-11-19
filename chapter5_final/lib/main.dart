71ae7a646ce59c7fdea89ad8e55b31d837803d7e
Damodar Lohani <dlohani48@gmail.com>
2021-11-19 09:40:24 +0545

    remove unused

--- chapter5_final/lib/main.dart
+++ chapter5_final/lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_academy/pages/about_page.dart';
-import 'package:flutter_academy/pages/contact_page.dart';
-import 'package:flutter_academy/pages/course_details_page.dart';
-import 'package:flutter_academy/pages/courses_page.dart';
-import 'package:flutter_academy/pages/error_404_page.dart';
-import 'package:flutter_academy/pages/home_page.dart';
 import 'package:flutter_academy/routes/app_route_parser.dart';
-import 'package:flutter_academy/routes/app_route_path.dart';
 import 'package:flutter_academy/routes/router_delegate.dart';
 
 void main() {
