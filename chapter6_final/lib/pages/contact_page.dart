fd5a8a925c542a18aeba5a68e6d5de16b30fd54c
Damodar Lohani <dlohani48@gmail.com>
2021-12-14 05:26:19 +0545

    chapter 6

---
+++ chapter6_final/lib/pages/contact_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_academy/res/responsive.dart';
+import 'package:flutter_academy/widgets/drawer_nav.dart';
+import 'package:flutter_academy/widgets/top_nav.dart';
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          TopNav(),
+        ],
+      ),
+      drawer: MediaQuery.of(context).size.width > ScreenSizes.md
+          ? null
+          : DrawerNav(),
+    );
+  }
+}
---
+++ chapter6_final/lib/widgets/top_nav.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_academy/main.dart';
+import 'package:flutter_academy/res/responsive.dart';
+
+class TopNav extends StatelessWidget {
+  const TopNav({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('Flutter Academy'),
+      elevation: kIsWeb ? 0 : null,
+      centerTitle: kIsWeb ? false : null,
+      actions: (MediaQuery.of(context).size.width <= ScreenSizes.md)
+          ? null
+          : [
+              TextButton(
+                child: Text("Home"),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: () {
+                  routerDelegate.go('/');
+                },
+              ),
+              TextButton(
+                child: Text("Courses"),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: () {
+                  routerDelegate.go('/courses');
+                },
+              ),
+              TextButton(
+                child: Text("About"),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: () {
+                  routerDelegate.go('/about');
+                },
+              ),
+              TextButton(
+                child: Text("Contact"),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: () {
+                  routerDelegate.go('/contact');
+                },
+              ),
+            ],
+    );
+  }
+}
