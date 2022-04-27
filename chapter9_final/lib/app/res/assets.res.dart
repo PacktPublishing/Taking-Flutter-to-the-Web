e7c882bc30fad4ff794ef8bd33d0e9c0e1fc0081
Damodar Lohani <dlohani48@gmail.com>
2022-04-27 06:00:40 +0545

    chapter 9

---
+++ chapter9_final/lib/app/res/assets.res.dart
@@ -0,0 +1,4 @@
+class Assets {
+  static const String instructor = 'assets/images/instructor.png';
+  static const String course = 'assets/images/course.png';
+}
---
+++ chapter9_final/lib/app/view_models/watchlist.vm.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_academy/infrastructure/model/course.model.dart';
+import 'package:flutter_academy/infrastructure/res/watchlist.service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'course.vm.dart';
+
+class WatchlistVM extends StateNotifier<List<CourseVM>> {
+  WatchlistService _watchlistService;
+
+  WatchlistVM(this._watchlistService) : super(const []) {
+    getWatchlist();
+  }
+
+  Future<void> addToWatchlist(int id, Course course) async {
+    await _watchlistService.addToWatchlist(id, course.toMap());
+    this.state = [CourseVM(course), ...this.state];
+  }
+
+  Future<void> removeFromWatchlist(int id) async {
+    await _watchlistService.removeFromWatchlist(id);
+    this.state = this.state.where((course) => course.course.id != id).toList();
+  }
+
+  Future<void> getWatchlist() async {
+    final watchlist = await _watchlistService.getWatchlist();
+    var courses = [];
+    for (final course in watchlist) {
+      courses = [CourseVM(Course.fromMap(course)), ...this.state];
+    }
+    this.state = [
+      ...courses
+    ];
+  }
+
+  bool isInWatchlist(int id) {
+    return this.state.where((element) => element.course.id == id).length != 0;
+  }
+}
+
+final watchlistVM = StateNotifierProvider<WatchlistVM, List<CourseVM>>(
+    (_) => WatchlistVM(WatchlistService.instance));
