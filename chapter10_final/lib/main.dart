3b105cac551bc60fc97eb7e21e4bd3d8fd25f17b
Damodar Lohani <dlohani48@gmail.com>
2022-06-14 11:15:01 +0545

    firebase configured

--- chapter10_final/lib/main.dart
+++ chapter10_final/lib/main.dart
@@ -4,9 +4,14 @@ import 'app/routes/app_route_parser.router.dart';
 import 'app/routes/router_delegate.router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Hive.initFlutter();
   runApp(ProviderScope(child: MyApp()));
 }
