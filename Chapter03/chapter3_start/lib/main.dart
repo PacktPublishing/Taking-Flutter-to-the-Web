308c6528ca81b9ec7cb14f38117be0a4befd074d
Damodar Lohani <dlohani48@gmail.com>
2022-09-09 20:54:49 +0545

    chapter 3 start

---
+++ Chapter03/chapter3_start/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_academy/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ Chapter03/chapter3_start/lib/widgets/responsive_gap.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_academy/res/responsive.dart';
+
+class ResponsiveGap extends StatelessWidget {
+  const ResponsiveGap({
+    Key? key,
+    required this.gap,
+  }) : super(key: key);
+
+  final double gap;
+
+  @override
+  Widget build(BuildContext context) {
+    return MediaQuery.of(context).size.width > ScreenSizes.md
+        ? SizedBox(width: gap)
+        : SizedBox(height: gap);
+  }
+}
